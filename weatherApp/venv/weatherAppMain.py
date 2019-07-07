ab019c5ac3945e1c1ad5b56c21874bb700bb833f
unknown <hongsoon793@gmail.com>
2019-07-07 11:00:47 +0800

    Python Tkinter Practice first commit

---
+++ weatherApp/venv/weatherAppMain.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from tkinter import font
+import requests
+
+HEIGHT = 500
+WIDTH = 600
+
+def test_function(entry):
+    print("This is the entry", entry)
+
+# api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+# 470554bd2270b211f24b0e4eaca9ccb6
+
+def get_weather(city):
+    weather_key = '470554bd2270b211f24b0e4eaca9ccb6'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
+    response = requests.get(url, params=params)
+    weather =response.json()
+
+    label['text'] = format_response(weather)
+
+
+def format_response(weather):
+    try:
+        name = weather['name']
+        desc = weather['weather'][0]['description']
+        temp = weather['main']['temp']
+        final_str =  'City: %s\nConditions: %s\nTemperature (°F): %s' %(name, desc, temp)
+        return final_str
+    except:
+        final_str='There was a problem retrieving the information.'
+        return final_str
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH) #create a canvas
+canvas.pack()
+
+background_image = tk.PhotoImage(file='landscape.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(x=0,y=0, relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='#80c1ff', bd=5) #create a frame, bd is border
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor= 'n') #relative sizing using %
+
+entry = tk.Entry(frame,  font=('Courier',10))
+entry.place(relwidth=0.65,relheight=1)
+
+
+button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get())) #lambda means the code doesnt only run at runtime but on every click
+button.place(relx=0.7, rely=0, relwidth= 0.3, relheight=1)
+
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame, font =('Segoe UI Semibold',15), anchor = 'nw', justify = 'left', bd=5)
+label.place(relwidth=1, relheight=1)
+
+#print(tk.font.families())
+
+root.mainloop()
+
+
